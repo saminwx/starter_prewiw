b8609f30d328e3d6cd3af0a4180bcca835f65add
Samin Goralija <samingoralija596@gmail.com>
2022-02-21 12:25:29 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    screens: {
+      'sm': '375px',
+      'lg': '1440px'
+    },
+    extend: {
+      fontFamely: {
+        Inter: 'sans-serif',
+        Lexend_Deca: 'Deca'
+         },
+         colors: {
+          blue: {
+          950: 	'#090b1a', 
+          1000: '	#1b1938' 
+          },
+          violet: {
+          950: '#aa5cdb'
+          }, 
+          },
+    },
+  },
+  plugins: [],
+}
